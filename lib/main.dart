3ca13a5d7ffaecc4f3386b49a89bb7d2985f872c
Aziz Akilbekov <akylbekovaziz60@gmail.com>
2021-11-10 15:29:00 +0600

    AzizUp

--- lib/main.dart
+++ lib/main.dart
@@ -49,3 +49,92 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+//void main() {
+//  runApp(MyApp());
+//}
+//class MyApp extends StatelessWidget {
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return MaterialApp(
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text(
+//            "aaaaaaaaaaa ",
+//            style: TextStyle(fontSize: 18.0),
+//          ),
+//        ),
+//        backgroundColor: Colors.blue[300],
+//        body: SafeArea(
+//          child: Center(
+//            child: Column(
+//              mainAxisAlignment: MainAxisAlignment.center,
+//              children: <Widget>[
+//                CircleAvatar(
+//                  radius: 80,
+//                  backgroundImage: AssetImage('images/protocoder.png'),
+//                ),
+//                Text(
+//                  'Proto Coders Point',
+//                  style: TextStyle(
+//                    fontFamily: 'SourceSansPro',
+//                    fontSize: 25,
+//                  ),
+//                ),
+//                Text(
+//                  'Welcome',
+//                  style: TextStyle(
+//                    fontSize: 20,
+//                    fontFamily: 'SourceSansPro',
+//                    color: Colors.red[400],
+//                    letterSpacing: 2.5,
+//                  ),
+//                ),
+//                SizedBox(
+//                  height: 20.0,
+//                  width: 200,
+//                  child: Divider(
+//                    color: Colors.teal[100],
+//                  ),
+//                ),
+//                Text("xxxxxxxxxxxxxxxxx"),
+//                Card(
+//                    color: Colors.white,
+//                    margin:
+//                    EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                    child: ListTile(
+//                      leading: Icon(
+//                        Icons.phone,
+//                        color: Colors.teal[900],
+//                      ),
+//                      title: Text(
+//                        'github',
+//                        style:
+//                        TextStyle(fontFamily: 'BalooBhai', fontSize: 20.0),
+//                      ),
+//                    )),
+//                Card(
+//                  color: Colors.white,
+//                  margin:
+//                  EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+//                  child: ListTile(
+//                    leading: Icon(
+//                      IconData(61042,fontFamily: 'MaterialIcons'),
+//                      color: Colors.teal[900],
+//                    ),
+//                    title: Text(
+//                      '190000000@stu.sdu.edu.kz',
+//                      style: TextStyle(fontSize: 20.0, fontFamily: 'Neucha'),
+//                    ),
+//                  ),
+//                )
+//              ],
+//            ),
+//          ),
+//        ),
+//      ),
+//    );
+//  }
+//}
